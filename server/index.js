ee3a06eaa6f292d017d2b95143d80ce5594888a4
Dhananjay Shinde <shinde3270@gmail.com>
2025-12-20 23:38:27 +0530

    WebSocket phase 1 working

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ const { Server } = require("socket.io"); // ⭐ NEW
 
 connectDB(); // Connect to MongoDB
 const socketServer = http.createServer(server);
-const io = new Server(socketServer);
+const io = new Server(socketServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
 
 const port = 3000;
 const corsOptions = {
@@ -29,7 +35,7 @@ server.use("/api/status", statusRoutes);
 server.use("/api/chat", chatRoutes);
 server.use("/api/message", messageRoutes);
 server.use("/api/homepage", homePageRoutes);
-io.on("connection", () => console.log("Socket Connected"));
+io.on("connection", (socket) => console.log(`Socket Connected ${socket.id}`));
 socketServer.listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`socketServer listening on port ${port}`);
 });
