05a9673a70773c306ee3209cff301e1a67f8be98
Dhananjay Shinde <shinde3270@gmail.com>
2026-03-01 18:23:33 +0530

    pick from env

--- server/index.js
+++ server/index.js
@@ -16,15 +16,15 @@ connectDB(); // Connect to MongoDB
 const socketServer = http.createServer(server);
 const io = new Server(socketServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST"],
   },
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true, // if you use cookies or authentication
 };
 server.use(express.json());
